8c19bbc8be5ca7c61e85fc6d6b3d5b9bfa1e034b
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-08-23 03:51:14 +0300

    Made up the header, styled it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='wrapper'>
+            <header>
+                <div className='headerLeft'>
+                    <img
+                        src='/img/logo.png'
+                        width={40}
+                        height={40}
+                        alt='logo sneakers'
+                    />
+                    <div className='headerInfo'>
+                        <h3>React Sneakers</h3>
+                        <p>Shop the best sneakers</p>
+                    </div>
+                </div>
+                <ul className='headerRight'>
+                    <li className='cart-box'>
+                        <img
+                            src='/img/Cart.svg'
+                            width={18}
+                            height={18}
+                            alt='Cart'
+                        />
+                        <span>45$</span>
+                    </li>
+                    <li>
+                        <img
+                            src='/img/Favorite-outline.svg'
+                            width={18}
+                            height={18}
+                            alt='User'
+                        />
+                    </li>
+                    <li>
+                        <img
+                            src='/img/User.svg'
+                            width={18}
+                            height={18}
+                            alt='User'
+                        />
+                    </li>
+                </ul>
+            </header>
+            <div className='content'>
+                <h1>All sneakers</h1>
+            </div>
+        </div>
+    );
 }
-
+// 2:09
 export default App;
