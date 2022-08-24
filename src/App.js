501ce30a592cc7c9cd03ecb6b7b24406324b1128
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-08-25 01:21:05 +0300

    Finished layout and styling of components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,82 @@
 function App() {
     return (
         <div className='wrapper'>
+            <div
+                // style={{ display: 'none' }}
+                className='overlay'
+            >
+                <div className='drawer'>
+                    <h2>
+                        Cart
+                        <img
+                            className='cart-item-btn___remove'
+                            src='/img/Btn-removed.svg'
+                            alt='Remove'
+                        />
+                    </h2>
+                    <div className='cart-item-box'>
+                        <div className='cart-item'>
+                            <div>
+                                <img
+                                    width={70}
+                                    height={70}
+                                    src='/img/sneakers/1.jpg'
+                                    alt='Sneakers'
+                                />
+                            </div>
+                            <div className='cart-item__text'>
+                                <p className='card-box__title'>
+                                    Nike Blazer Mid Suede Men's Sneakers
+                                </p>
+                                <b>750 $</b>
+                            </div>
+                            <img
+                                className='cart-item-btn___remove'
+                                src='/img/Btn-removed.svg'
+                                alt='Remove'
+                            />
+                        </div>
+                        <div className='cart-item'>
+                            <img
+                                width={70}
+                                height={70}
+                                src='/img/sneakers/1.jpg'
+                                alt='Sneakers'
+                            />
+                            <div className='cart-item__text'>
+                                <p className='card-box__title'>
+                                    Nike Blazer Mid Suede Men's Sneakers
+                                </p>
+                                <b>750 $</b>
+                            </div>
+                            <img
+                                className='cart-item-btn___remove'
+                                src='/img/Btn-removed.svg'
+                                alt='Remove'
+                            />
+                        </div>
+                    </div>
+                    <div className='cart-item-bottom'>
+                        <ul>
+                            <li>
+                                <span>Total:</span>
+                                <div></div>
+                                <b>1500 $</b>
+                            </li>
+                            <li className='cart-item-bottom__info'>
+                                <span>Discount:</span>
+                                <div></div>
+                                <b>0 %</b>
+                            </li>
+                        </ul>
+                        <button className='cart-item-btn greenButton'>
+                            Checkout
+                            <img src='/img/Arrow.svg' alt='Arrow' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
             <header>
                 <div className='headerLeft'>
                     <img
@@ -43,9 +119,18 @@ function App() {
                 </ul>
             </header>
             <div className='content'>
-                <h1>All sneakers</h1>
-                <div className='content-box'>
+                <div className='content-header'>
+                    <h1>All sneakers</h1>
+                    <div className='search-block'>
+                        <img src='/img/Search.svg' alt='Search icon' />
+                        <input placeholder='Search...' />
+                    </div>
+                </div>
+                <div className='content-card-box'>
                     <div className='card-box'>
+                        <div className='favorite'>
+                            <img src='/img/Heart-unliked.svg' alt='Unliked' />
+                        </div>
                         <img src='/img/sneakers/1.jpg' alt='green sneakers' />
                         <p className='card-box__title'>
                             Nike Blazer Mid Suede Men's Sneakers
@@ -128,31 +213,10 @@ function App() {
                             </button>
                         </div>
                     </div>
-
-                    {/* <div className='card-box'>
-                        <img src='/img/sneakers/5.jpg' alt='green sneakers' />
-                        <p className='card-box__title'>
-                            Nike Blazer Mid Suede Men's Sneakers
-                        </p>
-                        <div className='card-box__info'>
-                            <div className='card-box__text'>
-                                <span>Price:</span>
-                                <b>750 $</b>
-                            </div>
-                            <button className='card-box__btn'>
-                                <img
-                                    width={11}
-                                    height={11}
-                                    src='/img/Plus.svg'
-                                    alt='plus'
-                                />
-                            </button>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
     );
 }
-// 2:09
+// #2 1:20
 export default App;
