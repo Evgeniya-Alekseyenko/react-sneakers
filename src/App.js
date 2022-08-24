20e378c5e673117f99b469357294b8a986a62a75
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-08-24 15:23:28 +0300

    Made and styled product cards

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
                             height={20}
                             alt='Cart'
                         />
-                        <span>45$</span>
+                        <span>45 $</span>
                     </li>
                     <li>
                         <img
@@ -44,6 +44,112 @@ function App() {
             </header>
             <div className='content'>
                 <h1>All sneakers</h1>
+                <div className='content-box'>
+                    <div className='card-box'>
+                        <img src='/img/sneakers/1.jpg' alt='green sneakers' />
+                        <p className='card-box__title'>
+                            Nike Blazer Mid Suede Men's Sneakers
+                        </p>
+                        <div className='card-box__info'>
+                            <div className='card-box__text'>
+                                <span>Price:</span>
+                                <b>750 $</b>
+                            </div>
+                            <button className='card-box__btn'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/Plus.svg'
+                                    alt='plus'
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className='card-box'>
+                        <img src='/img/sneakers/2.jpg' alt='green sneakers' />
+                        <p className='card-box__title'>
+                            Nike Blazer Mid Suede Men's Sneakers
+                        </p>
+                        <div className='card-box__info'>
+                            <div className='card-box__text'>
+                                <span>Price:</span>
+                                <b>750 $</b>
+                            </div>
+                            <button className='card-box__btn'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/Plus.svg'
+                                    alt='plus'
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className='card-box'>
+                        <img src='/img/sneakers/3.jpg' alt='green sneakers' />
+                        <p className='card-box__title'>
+                            Nike Blazer Mid Suede Men's Sneakers
+                        </p>
+                        <div className='card-box__info'>
+                            <div className='card-box__text'>
+                                <span>Price:</span>
+                                <b>750 $</b>
+                            </div>
+                            <button className='card-box__btn'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/Plus.svg'
+                                    alt='plus'
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className='card-box'>
+                        <img src='/img/sneakers/4.jpg' alt='green sneakers' />
+                        <p className='card-box__title'>
+                            Nike Blazer Mid Suede Men's Sneakers
+                        </p>
+                        <div className='card-box__info'>
+                            <div className='card-box__text'>
+                                <span>Price:</span>
+                                <b>750 $</b>
+                            </div>
+                            <button className='card-box__btn'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/Plus.svg'
+                                    alt='plus'
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* <div className='card-box'>
+                        <img src='/img/sneakers/5.jpg' alt='green sneakers' />
+                        <p className='card-box__title'>
+                            Nike Blazer Mid Suede Men's Sneakers
+                        </p>
+                        <div className='card-box__info'>
+                            <div className='card-box__text'>
+                                <span>Price:</span>
+                                <b>750 $</b>
+                            </div>
+                            <button className='card-box__btn'>
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src='/img/Plus.svg'
+                                    alt='plus'
+                                />
+                            </button>
+                        </div>
+                    </div> */}
+                </div>
             </div>
         </div>
     );
